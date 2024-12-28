3220aacb844a656fa6d9f80289db6d8739b97576
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-12-28 15:53:40 +0900

    Update installation.sh
    Signed-off-by: Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>

--- installation.sh
+++ installation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Streamlit과 LLM 관련 라이브러리 설치
-pip install streamlit openai langchain-openai langchain-huggingface langchain-community PyPDF2 pypdf pymupdf faiss-cpu
+pip install streamlit openai langchain_openai langchain_huggingface langchain-openai langchain-huggingface langchain-community PyPDF2 pypdf pymupdf faiss-cpu
  
